53df3c258625d887ba62afc2c954d92126110ced
dvelopWorld <119862346+dvelopWorld@users.noreply.github.com>
2022-12-07 14:09:40 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	"development": {
 		"username": "root",
-		"password": '123456',
+		"password": 'password',
 		"database": "hotelroom",
 		"host": "localhost",
 		"dialect": "mysql"
